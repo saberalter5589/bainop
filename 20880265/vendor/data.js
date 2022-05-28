d739f1833a19ad6bdca53609bc5eb2230fd105aa
saberalter5589 <36893244+saberalter5589@users.noreply.github.com>
2022-05-28 08:39:48 +0700

    Add files via upload

---
+++ 20880265/vendor/data.js
@@ -0,0 +1,20 @@
+
+function getInfo() {
+    return [
+        {
+            title: 'For those about to rock...',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore modi quasi tempora nesciunt laborum quo temporibus? Consectetur saepe optio sequi tempore? Omnis iste, exercitationem quasi eos eveniet neque vitae libero.',
+            imagepath: 'images/01.jpg'
+        },
+        {
+            title: 'We salute you!',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa modi, voluptas tenetur dolorum libero est neque totam dolor quia, perspiciatis, dolores eligendi quos maxime suscipit itaque! Dicta explicabo incidunt optio?',
+            imagepath: 'images/02.jpg'
+        },
+        {
+            title: 'Let there be rock!',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, aspernatur quia architecto eveniet id dicta velit provident, dolore officia quaerat voluptatibus nostrum sunt minus cumque explicabo quas commodi labore blanditiis!',
+            imagepath: 'images/03.jpg'
+        }
+    ]
+}
